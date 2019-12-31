40b178330b9cbb198b727eeb23bf8d10cad00414
siwoo-lim <45985794+siwoo-lim@users.noreply.github.com>
2020-01-01 04:17:43 +0900

    Add files via upload

--- message.js
+++ message.js
@@ -1,20 +1,51 @@
-var isPushed = false;
+var pushedCnt = 0;
+var hideList = document.getElementsByClassName("hide");
+var message = '이거 만드는데 진짜 힘들었다구요 ㅡㅅㅡ';
+var i = 0;
+var send = null;
+var sendC = null;
 
 function sendMessage(){
-  if(isPushed) return 0;
-  else {
-    document.querySelector(".send_text").classList.add('hide');
+  if(pushedCnt > 1) return 0;
+  else if(pushedCnt == 1){
+    send.classList.add('hide');
+    document.getElementsByClassName("text_message_me")[1].innerHTML = '<div class="child">'+message+'</div>';
+    hideList[0].setAttribute('class', 'wow bounceInRight animated');
+    setTimeout(next_message, 1000);
+    pushedCnt++;
+  } else {
+    send = document.querySelector(".send_text");
+    send.classList.add('hide');
     display_effect();
-    isPushed = true;
+    pushedCnt++;
+    setTimeout(next_message, 1000);
+    setTimeout(typing, 2000);
   }
 }
 
 function display_effect(){
-  var hideList = document.getElementsByClassName("hide");
     for(var i=0; i<2; i++){
       hideList[0].setAttribute('class', 'wow bounceInRight animated');
   }
+}
 
+function next_message(){
+  hideList[0].setAttribute('class', 'wow bounceInLeft animated');
+}
+
+function typing() {
+  sendC  = document.getElementsByClassName("send_text")[0];
+  send.classList.remove('hide');
+  sendC.innerHTML = '&nbsp&nbsp&nbsp';
+  setTimeout(typeWriter, 0);
+}
+
+function typeWriter() {
+  if(i<message.length){
+    sendC.innerHTML += message.charAt(i);
+    i++
+    setTimeout(typeWriter, 50);
+  }
 }
 
 function wait(ms){
